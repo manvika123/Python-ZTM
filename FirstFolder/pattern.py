545e73ab3b8e2f395884c2cae92580aacd552845
Manvika Jaiswal <manvikajaiswal1995@gmail.com>
2020-06-29 19:03:34 +0000

    Christmas Tree Pattern

---
+++ FirstFolder/pattern.py
@@ -0,0 +1,18 @@
+#Exercise!
+#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+picture = [
+  [0,0,0,1,0,0,0],
+  [0,0,1,1,1,0,0],
+  [0,1,1,1,1,1,0],
+  [1,1,1,1,1,1,1],
+  [0,0,0,1,0,0,0],
+  [0,0,0,1,0,0,0]
+]
+for row in picture:
+  for pixcel in row:
+    if(pixcel == 1):
+      print ('*',end= '')
+    else:
+      print(' ',end= '')
+  print('')
+  
